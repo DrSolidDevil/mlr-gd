b1df44c5cf7d8461efbe168c9d25c92468e61373
DrSolidDevil <51828495+drsoliddevil@users.noreply.github.com>
2025-01-09 20:00:30 +0100

    fixed source

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_packages, setup
 
-with open("app/README.md", "r") as f:
+with open("src/README.md", "r") as f:
     long_description = f.read()
 
 setup(
-    name="mlr-gd",
-    version="0.0",
+    name="src",
+    version="0.0.1",
     description="A package for multiple linear regression by gradient descent.",
-    package_dir={"": "app"},
-    packages=find_packages(where="app"),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/DrSolidDevil/mlr-gd/",
@@ -16,12 +16,12 @@ setup(
     license="TO BE DETERMINED",
     classifiers=[
         "License :: OSI Approved :: TO BE DETERMINED",
-        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
     ],
     install_requires=["numpy >= 2.2.1"],
     extras_require={
-        "dev": ["pytest>=7.0", "twine>=4.0.2"],  # To be determined
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],  # To be determined if necessary to include
     },
     python_requires=">=3.11",
 )
