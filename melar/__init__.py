a22ae6e635801321e1810c4d85bf44871b7d0980
RedDingleBerry <196618136+RedDingleBerry@users.noreply.github.com>
2025-01-29 10:38:41 +0100

    Added metadata to melar/__init__.py

--- melar/__init__.py
+++ melar/__init__.py
@@ -1,2 +1,14 @@
+"""Melar is the module for mlr-gd.
+"""
 from melar.main import LinearRegression
 from melar import cfuncs
+
+__author__ = ["DrSolidDevil"]
+__copyright__ = "Copyright (c) 2025, DrSolidDevil"
+__credits__ = ["DrSolidDevil", "RedDingleBerry"]
+__license__ = "BSD 3-Clause"
+__maintainer__ = ["DrSoliDevil"]
+__repository__ = "https://github.com/DrSolidDevil/mlr-gd"
+__status__ = "Development"
+__version__ = '0.2.0'
+__version_info__ = (0, 2, 0)
