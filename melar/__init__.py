513fa7c19916b5ffb307aa3190e553a302539903
RedDingleBerry <196618136+RedDingleBerry@users.noreply.github.com>
2025-01-29 10:57:51 +0100

    Forgot dash. (fix)

--- melar/__init__.py
+++ melar/__init__.py
@@ -6,7 +6,7 @@ from melar import cfuncs
 __author__ = ["DrSolidDevil"]
 __copyright__ = "Copyright (c) 2025, DrSolidDevil"
 __credits__ = ["DrSolidDevil", "RedDingleBerry"]
-__license__ = "BSD 3-Clause"
+__license__ = "BSD-3-Clause"
 __maintainer__ = ["DrSoliDevil"]
 __status__ = "Development"
 __version__ = '0.2.0'
