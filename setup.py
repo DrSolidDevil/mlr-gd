5038c8e3e9d3efe5624b1e9162aa427f01c582d1
DrSolidDevil <51828495+drsoliddevil@users.noreply.github.com>
2025-02-13 20:36:16 +0100

    changed theme requirement to pydata-sphinx-theme

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     install_requires=["numpy >= 2.2.1"],
     extras_require={
         "dev": ["twine>=6.0.1", "pandas>=2.2.3", "pytest>=8.3.4", "setuptools>=75.8.0"],
-        "docs": ["sphinx-rtd-theme >= 3.0.2", "Sphinx >= 8.1.3", "myst-parser >= 4.0.0"]
+        "docs": ["pydata-sphinx-theme >= 3.0.2", "Sphinx >= 8.1.3", "myst-parser >= 4.0.0"]
     },
     python_requires=">=3.11",
 )
