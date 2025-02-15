b2ded5ae8dd83688c5f5640e9e29aa58c9d92204
DrSolidDevil <51828495+DrSolidDevil@users.noreply.github.com>
2025-02-15 18:46:29 +0100

    changed status to production

--- melar/__init__.py
+++ melar/__init__.py
@@ -8,6 +8,6 @@ __copyright__ = "Copyright (c) 2025, DrSolidDevil"
 __credits__ = ["DrSolidDevil", "RedDingleBerry"]
 __license__ = "BSD-3-Clause"
 __maintainer__ = ["DrSoliDevil"]
-__status__ = "Development"
+__status__ = "Production"
 __version__ = '0.3.0'
 __version_info__ = (0, 3, 0)
