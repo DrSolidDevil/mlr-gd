1be01fe11378a8ecc5c6ba85bac3e3666b4fdf44
DrSolidDevil <51828495+drsoliddevil@users.noreply.github.com>
2025-04-18 14:45:55 +0200

    Added evaluation import and changed status and version

--- melar/__init__.py
+++ melar/__init__.py
@@ -2,12 +2,13 @@
 """
 from melar.linear_regression import LinearRegression
 from melar import cfuncs
+from melar import evaluation
 
 __author__ = ["DrSolidDevil"]
 __copyright__ = "Copyright (c) 2025, DrSolidDevil"
 __credits__ = ["DrSolidDevil", "RedDingleBerry"]
 __license__ = "BSD-3-Clause"
 __maintainer__ = ["DrSoliDevil"]
-__status__ = "Production"
-__version__ = '0.3.0'
+__status__ = "Development"
+__version__ = '0.4.0'
 __version_info__ = (0, 3, 0)
