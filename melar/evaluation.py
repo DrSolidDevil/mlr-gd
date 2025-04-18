03e79eefc033bb1cad3f67e9ec1c324b14333ebf
DrSolidDevil <51828495+drsoliddevil@users.noreply.github.com>
2025-04-18 13:24:14 +0200

    Created evaluation.py that contains evaluation functionality, most prominently r2 score.

---
+++ melar/evaluation.py
@@ -0,0 +1,50 @@
+import numpy as np
+from linear_regression import LinearRegression
+
+
+def tss(y) -> np.float32:
+    """Total sum of squares
+
+    Args:
+        y (np.ndarray | pd.DataFrame | pd.Series): Target values
+    """
+    if type(y).__name__ == "DataFrame":
+        y = y.T
+    return np.sum(np.square(y - np.mean(y)))
+
+def rss(model: LinearRegression, x, y):
+    """Residual Sum of Squares
+        Args:
+            model (LinearRegression): Model to be evaluated
+            x (np.ndarray | pd.DataFrame | pd.Series): Input values
+            y (np.ndarray | pd.DataFrame | pd.Series): Target values
+    """
+    if type(x).__name__ == "DataFrame":
+        x = y.T
+    if type(y).__name__ == "DataFrame":
+        y = y.T
+    return np.sum(np.square(y - model.predict(x)))
+
+
+def r2_score(model: LinearRegression, x, y):
+    """Calculate coefficient of determination for model.
+
+    Args:
+        model (LinearRegression): Model to be evaluated
+        x (np.ndarray | pd.DataFrame | pd.Series): Input Values
+        y (np.ndarray | pd.DataFrame | pd.Series): Target Values
+    """
+    return 1 - rss(model, x, y)/tss(y)
+
+
+
+
+
+
+
+
+
+
+
+
+
